bec7781d615d7cd5312d2b22f4f865de2ef4971c
ak-WJK <WJK2288@163.com>
2017-06-02 21:17:02 +0800

    灰点的实现

--- app/src/main/java/com/atguigu/beijingnewsdemo/activity/GuideActivity.java
+++ app/src/main/java/com/atguigu/beijingnewsdemo/activity/GuideActivity.java
@@ -13,6 +13,7 @@ import android.widget.LinearLayout;
 
 import com.atguigu.beijingnewsdemo.MainActivity;
 import com.atguigu.beijingnewsdemo.R;
+import com.atguigu.beijingnewsdemo.utils.DensityUtil;
 
 import java.util.ArrayList;
 
@@ -45,21 +46,44 @@ public class GuideActivity extends AppCompatActivity {
         setContentView(R.layout.activity_guide);
         ButterKnife.bind(this);
 
+        initView();
+
+
+    }
+
+    private void initView() {
         imageViews = new ArrayList<>();
         for (int i = 0; i < icons.length; i++) {
 
             ImageView imageView = new ImageView(this);
-
+            //设置imageview的背景色
             imageView.setBackgroundResource(icons[i]);
-
+            //添加imageview到集合
             imageViews.add(imageView);
+
+            //点的实现
+            ImageView point = new ImageView(this);
+            //设置点的背景色
+            point.setBackgroundResource(R.drawable.guide_point_normal);
+
+            LinearLayout.LayoutParams perams = new LinearLayout.LayoutParams(DensityUtil.dip2px(this, 10), DensityUtil.dip2px(this, 10));
+            //设置点的大小
+            point.setLayoutParams(perams);
+            //设置点之间的距离
+            if (i != 0) {
+                perams.leftMargin = 20;
+
+            }
+
+            //装配到线性布局中
+            llPoint.addView(point);
+
 
         }
 
         viewpager.setAdapter(new MyAdapter());
-
-
     }
+
 
     class MyAdapter extends PagerAdapter {
 
