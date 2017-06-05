686f4953f8313df28dc809ec5f7b05a32e108b51
ak-WJK <WJK2288@163.com>
2017-06-06 00:58:12 +0800

    设置指定页面可以划出菜单栏

--- app/src/main/java/com/atguigu/beijingnewsdemo/view/NoScrollViewPager.java
+++ app/src/main/java/com/atguigu/beijingnewsdemo/view/NoScrollViewPager.java
@@ -11,15 +11,42 @@ import android.view.MotionEvent;
 
 public class NoScrollViewPager extends ViewPager {
 
+    private float downX;
+
     public NoScrollViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
 
-
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
-        getParent().requestDisallowInterceptTouchEvent(true);
+
+
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                getParent().requestDisallowInterceptTouchEvent(true);
+                downX = ev.getRawX();
+
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float moveX = ev.getRawX();
+                if (moveX - downX > 0 && getCurrentItem() == 0) {
+                    getParent().requestDisallowInterceptTouchEvent(false);
+
+                } else if (downX - moveX > 0 && getCurrentItem() == getAdapter().getCount() - 1) {
+                    getParent().requestDisallowInterceptTouchEvent(true);
+                } else {
+                    getParent().requestDisallowInterceptTouchEvent(true);
+                }
+
+
+                break;
+            case MotionEvent.ACTION_UP:
+
+                break;
+        }
+
+
         return super.dispatchTouchEvent(ev);
     }
 }
